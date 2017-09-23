6ae368c21123d5b3618f9dc1e469e09de26fa507
Darnell Gillead <dlgillead@gmail.com>
2017-09-23 15:45:11 -0500

    created checkPalindrome function

--- src/PalindromeInput.js
+++ src/PalindromeInput.js
@@ -4,18 +4,25 @@ class PalindromeInput extends Component {
   constructor(props) {
     super(props);
 
-    this.handleInputChange = this.handleInputChange.bind(this);
+    this.checkPalindrome = this.checkPalindrome.bind(this);
   }
 
-  handleInputChange(e) {
-    console.log(e.target.value);
+  checkPalindrome(e) {
+    let currentString = e.target.value;
+    for (let i = 0; i < currentString.length / 2; i++) {
+      if (currentString[i] === currentString[currentString.length - (i + 1)]) {
+        console.log('palindrome');
+      } else {
+        console.log('not a palindrome');
+      }
+    }
   }
 
   render() {
     return (
       <div>
         <form id="pali-input">
-          <input type="text" name="palindrome" onChange={this.handleInputChange}></input>
+          <input type="text" name="palindrome" onChange={this.checkPalindrome}></input>
         </form>
       </div>
     )
